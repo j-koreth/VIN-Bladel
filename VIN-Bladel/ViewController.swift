b6daad2fa61713bb99b801629a32ddec0df6079b
Joel Koreth <joelsymon@gmail.com>
2017-12-01 08:46:45 -0600

    Initial commit

---
+++ VIN-Bladel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VIN-Bladel
+//
+//  Created by Joel Koreth on 12/1/17.
+//  Copyright © 2017 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
