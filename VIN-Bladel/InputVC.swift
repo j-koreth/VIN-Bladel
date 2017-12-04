ca5b83d0b2ce5cae51374a733ea6d6788913a130
Alisha F <afangg91@gmail.com>
2017-12-04 12:15:55 -0600

    Camera test

---
+++ VIN-Bladel/InputVC.swift
@@ -0,0 +1,20 @@
+//
+//  InputVC.swift
+//  VIN-Bladel
+//
+//  Created by Alisha Fong on 12/4/17.
+//  Copyright © 2017 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class InputVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ VIN-Bladel/ScanVC.swift
@@ -0,0 +1,95 @@
+//
+//  ScanVC.swift
+//  VIN-Bladel
+//
+//  Created by Alisha Fong on 12/4/17.
+//  Copyright © 2017 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class CameraView: UIView {
+    override class var layerClass: AnyClass {
+        get {
+            return AVCaptureVideoPreviewLayer.self
+        }
+    }
+    
+    override var layer: AVCaptureVideoPreviewLayer {
+        get {
+            return super.layer as! AVCaptureVideoPreviewLayer
+        }
+    }
+}
+
+class ScanVC: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+
+    var session = AVCaptureSession()
+    var sessionQueue = DispatchQueue(label: AVCaptureSession.self.description())
+    var cameraView: CameraView!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        cameraView = CameraView()
+        
+        view = cameraView
+        
+        session.beginConfiguration()
+        
+        let videoDevice = AVCaptureDevice.default(for: .video)
+        
+        if (videoDevice != nil) {
+            let videoDeviceInput = try? AVCaptureDeviceInput(device: videoDevice!)
+            
+            if (videoDeviceInput != nil) {
+                if (session.canAddInput(videoDeviceInput!)) {
+                    session.addInput(videoDeviceInput!)
+                }
+            }
+            
+            let metadataOutput = AVCaptureMetadataOutput()
+            
+            if (session.canAddOutput(metadataOutput)) {
+                session.addOutput(metadataOutput)
+                
+                metadataOutput.metadataObjectTypes = [
+                    .ean13,
+                    .qr
+                ]
+                
+                metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            }
+        }
+        
+        session.commitConfiguration()
+        
+        cameraView.layer.session = session
+        cameraView.layer.videoGravity = .resizeAspectFill
+        
+        let videoOrientation: AVCaptureVideoOrientation
+        switch UIApplication.shared.statusBarOrientation {
+        case .portrait:
+            videoOrientation = .portrait
+            
+        case .portraitUpsideDown:
+            videoOrientation = .portraitUpsideDown
+            
+        case .landscapeLeft:
+            videoOrientation = .landscapeLeft
+            
+        case .landscapeRight:
+            videoOrientation = .landscapeRight
+            
+        default:
+            videoOrientation = .portrait
+        }
+        
+        cameraView.layer.connection?.videoOrientation = videoOrientation
+
+    }
+
+
+}
