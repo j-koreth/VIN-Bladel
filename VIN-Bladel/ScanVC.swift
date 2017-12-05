affc9230713403ea8e65680a32c2566fb931a951
Olivia Marunde <omarunde8029@jh-mba158-10.d214.org>
2017-12-05 09:02:56 -0600

    -om

--- VIN-Bladel/ScanVC.swift
+++ VIN-Bladel/ScanVC.swift
@@ -9,114 +9,18 @@
 import UIKit
 import AVFoundation
 
-class CameraView: UIView {
-    override class var layerClass: AnyClass {
-        get {
-            return AVCaptureVideoPreviewLayer.self
-        }
-    }
+class CameraView: UIViewController {
+    let API_KEY = "https://vpic.nhtsa.dot.gov/api/vehicles/decodevinvalues/5UXWX7C5*BA?format=json"
     
-    override var layer: AVCaptureVideoPreviewLayer {
-        get {
-            return super.layer as! AVCaptureVideoPreviewLayer
-        }
-    }
-}
-
-class ScanVC: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
-
-    var session = AVCaptureSession()
-    var sessionQueue = DispatchQueue(label: AVCaptureSession.self.description())
-    var cameraView: CameraView!
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        cameraView = CameraView()
-        
-        view = cameraView
-        
-        session.beginConfiguration()
-        
-        let videoDevice = AVCaptureDevice.default(for: .video)
-        
-        if (videoDevice != nil) {
-            let videoDeviceInput = try? AVCaptureDeviceInput(device: videoDevice!)
-            
-            if (videoDeviceInput != nil) {
-                if (session.canAddInput(videoDeviceInput!)) {
-                    session.addInput(videoDeviceInput!)
-                }
-            }
-            
-            let metadataOutput = AVCaptureMetadataOutput()
-            
-            if (session.canAddOutput(metadataOutput)) {
-                session.addOutput(metadataOutput)
-                
-                metadataOutput.metadataObjectTypes = [
-                    .ean13,
-                    .qr
-                ]
-                
-                metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
-            }
         }
-        
-        session.commitConfiguration()
-        
-        cameraView.layer.session = session
-        cameraView.layer.videoGravity = .resizeAspectFill
-        
-        let videoOrientation: AVCaptureVideoOrientation
-        switch UIApplication.shared.statusBarOrientation {
-        case .portrait:
-            videoOrientation = .portrait
-            
-        case .portraitUpsideDown:
-            videoOrientation = .portraitUpsideDown
-            
-        case .landscapeLeft:
-            videoOrientation = .landscapeLeft
-            
-        case .landscapeRight:
-            videoOrientation = .landscapeRight
-            
-        default:
-            videoOrientation = .portrait
-        }
-        
-        cameraView.layer.connection?.videoOrientation = videoOrientation
-
-    }
-
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        sessionQueue.async {
-            self.session.startRunning()
-        }
-    }
     
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        sessionQueue.async {
-            self.session.stopRunning()
-        }
-    }
-    
-    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
-        if (metadataObjects.count > 0 && metadataObjects.first is AVMetadataMachineReadableCodeObject) {
-            let scan = metadataObjects.first as! AVMetadataMachineReadableCodeObject
-            
-            let alertController = UIAlertController(title: "Barcode Scanned", message: scan.stringValue, preferredStyle: .alert)
-            
-            alertController.addAction(UIAlertAction(title: "OK", style: .default, handler:nil))
-            
-            present(alertController, animated: true, completion: nil)
-        }
-    }
 
+
+    
 }
+
+
