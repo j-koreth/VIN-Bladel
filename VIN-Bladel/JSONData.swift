d47a1b62c4f4e422d9f937900077fd1f7ed1be3a
Joel Koreth <joelsymon@gmail.com>
2017-12-06 09:01:19 -0600

    added function

--- VIN-Bladel/JSONData.swift
+++ VIN-Bladel/JSONData.swift
@@ -8,6 +8,22 @@
 
 import Foundation
 
-func JSONData(){
-    
+func JSONData(VINNumber : String){
+    let urlString = "https://vpic.nhtsa.dot.gov/api/vehicles/decodevinvalues/\(VINNumber)?format=json"
+    let url = URL(string: urlString)!
+    URLSession.shared.dataTask(with: url) { (myData, response, error) in
+        if let JSONObject = try? JSONSerialization.jsonObject(with: myData!, options: .allowFragments) as! NSDictionary{
+            //                print(JSONObject)
+            let results = JSONObject.object(forKey: "Results") as! NSArray
+            print(results)
+            print(VINNumber)
+            let modelyear = results.value(forKey: "ModelYear") as! NSArray
+            print(modelyear[0])
+            let make = results.value(forKey: "Make") as! NSArray
+            print(make[0])
+            let model = results.value(forKey: "Model") as! NSArray
+            print(model[0])
+            
+        }
+        }.resume()
 }
