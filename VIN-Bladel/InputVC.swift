31b6752001fafc44bacf29ece39ec17a17a3665f
Alisha F <afangg91@gmail.com>
2017-12-18 08:21:49 -0600

    idk what i did - af

--- VIN-Bladel/InputVC.swift
+++ VIN-Bladel/InputVC.swift
@@ -10,22 +10,37 @@ import UIKit
 
 class InputVC: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var searchButton: UIButton!
     @IBOutlet weak var vinTextfield: UITextField!
+    
     var barcode = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.barStyle = UIBarStyle.black
+        searchButton.isEnabled = false
+        searchButton.backgroundColor = UIColor.clear
         
-        
-
 
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         view.endEditing(true)
-        barcode = vinTextfield.text!
+        
+        if vinTextfield.text!.count >= 17
+        {
+            barcode = vinTextfield.text!
+        }
+        
         return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if vinTextfield.text!.count >= 17
+        {
+            searchButton.isEnabled = true
+            
+        }
     }
 
 
