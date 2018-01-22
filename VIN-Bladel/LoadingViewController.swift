502a1cfd5bfdd4b45206bbf638378098b6d18002
Will Stiefbold <wstiefbold0444@jh-mba158-13.d214.org>
2018-01-22 08:55:02 -0600

    added new file and did storyboard stuffs ws

---
+++ VIN-Bladel/LoadingViewController.swift
@@ -0,0 +1,86 @@
+//
+//  LoadingViewController.swift
+//  VIN-Bladel
+//
+//  Created by Will Stiefbold on 1/22/18.
+//  Copyright © 2018 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class LoadingViewController: UIViewController {
+    
+    var array = [UIImage]()
+    
+    var image1 = UIImage(named: "1")
+    var image2 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+//    var image1 = UIImage(named: "1")
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+
+
+
+}
