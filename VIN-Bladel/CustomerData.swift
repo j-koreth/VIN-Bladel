8de282de9328ff1a8a38ca029ebb9f969dd4ca8f
Alisha F <afangg91@gmail.com>
2018-01-23 08:53:26 -0600

    customerData

---
+++ VIN-Bladel/CustomerData.swift
@@ -0,0 +1,30 @@
+//
+//  customerData.swift
+//  VIN-Bladel
+//
+//  Created by Alisha Fong on 1/23/18.
+//  Copyright © 2018 John Hersey High School. All rights reserved.
+//
+
+import Foundation
+
+class CustomerData
+{
+    var customerID: String!
+    var customerTitle: String!
+    var customerFirst: String!
+    var customerLast: String!
+
+    var customerAddress1: String!
+    var customerAddress2: String!
+    var customerCity: String!
+    var customerState: String!
+    var customerZip: String!
+    var customerCountry: String!
+    var customerEmail: String!
+    var customerHomePhone: String!
+    var customerWorkPhone: String!
+    
+    
+
+}
