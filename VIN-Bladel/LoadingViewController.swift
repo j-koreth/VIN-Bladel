385d8cd9dac94d714bfcb8a929fc9b8b1a16570c
Will Stiefbold <wstiefbold0444@jh-mba158-13.d214.org>
2018-01-23 08:41:21 -0600

    picture are up

--- VIN-Bladel/LoadingViewController.swift
+++ VIN-Bladel/LoadingViewController.swift
@@ -14,19 +14,19 @@ class LoadingViewController: UIViewController {
     
     var image1 = UIImage(named: "1")
     var image2 = UIImage(named: "2")
-//    var image1 = UIImage(named: "1")
-//    var image1 = UIImage(named: "1")
-//    var image1 = UIImage(named: "1")
-//    var image1 = UIImage(named: "1")
-//    var image1 = UIImage(named: "1")
-//    var image1 = UIImage(named: "1")
-//    var image1 = UIImage(named: "1")
-//    var image1 = UIImage(named: "1")
-//    var image1 = UIImage(named: "1")
-//    var image1 = UIImage(named: "1")
-//    var image1 = UIImage(named: "1")
-//    var image1 = UIImage(named: "1")
-//    var image1 = UIImage(named: "1")
+    var image3 = UIImage(named: "3")
+    var image4 = UIImage(named: "4")
+    var image5 = UIImage(named: "5")
+    var image6 = UIImage(named: "6")
+    var image7 = UIImage(named: "7")
+    var image8 = UIImage(named: "8")
+    var image9 = UIImage(named: "9")
+    var image10 = UIImage(named: "10")
+    var image11 = UIImage(named: "11")
+    var image12 = UIImage(named: "12")
+    var image13 = UIImage(named: "13")
+    var image14 = UIImage(named: "14")
+    var image15 = UIImage(named: "15")
 //    var image1 = UIImage(named: "1")
 //    var image1 = UIImage(named: "1")
 //    var image1 = UIImage(named: "1")
@@ -78,7 +78,8 @@ class LoadingViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
+        array.append(image1!)
+        
     }
 
 
