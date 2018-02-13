5678b5d34afd3e082acedb2b4c0fb0d5ac23d7e5
Alisha F <afangg91@gmail.com>
2018-02-13 12:32:45 -0600

    fixed buttons and added fonts

--- VIN-Bladel/VC2/DataNotFoundViewController.swift
+++ VIN-Bladel/VC2/DataNotFoundViewController.swift
@@ -10,12 +10,21 @@ import UIKit
 
 class DataNotFoundViewController: UIViewController {
     
+    @IBOutlet weak var newCustomerButton: UIButton!
+    @IBOutlet weak var previousCustomerButton: UIButton!
+    
     var customerArray = CustomerDB()
     var vehicleDB = VehicleDatabase()
     var carData = VehicleData()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        newCustomerButton.backgroundColor = UIColor(red:0.32, green:0.58, blue:0.79, alpha:1.0)
+        newCustomerButton.layer.cornerRadius = 60
+        
+        previousCustomerButton.backgroundColor = UIColor(red:0.32, green:0.58, blue:0.79, alpha:1.0)
+        previousCustomerButton.layer.cornerRadius = 60
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
