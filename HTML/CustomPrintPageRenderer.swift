9fcd48585a0a4c5eb8c4826535f635e9a0260692
Alisha F <afangg91@gmail.com>
2018-04-30 08:59:51 -0500

    Added Parts VC

--- HTML/CustomPrintPageRenderer.swift
+++ HTML/CustomPrintPageRenderer.swift
@@ -1,29 +1,29 @@
+////
+////  CustomPrintPageRenderer.swift
+////  VIN-Bladel
+////
+////  Created by Olivia Marunde on 4/18/18.
+////  Copyright © 2018 John Hersey High School. All rights reserved.
+////
 //
-//  CustomPrintPageRenderer.swift
-//  VIN-Bladel
+//import UIKit
 //
-//  Created by Olivia Marunde on 4/18/18.
-//  Copyright © 2018 John Hersey High School. All rights reserved.
+//class CustomPrintPageRenderer: UIPrintPageRenderer {
+//    
+//    let letterPageWidth: CGFloat = 612
+//    let letterPageHeight: CGFloat = 792
 //
-
-import UIKit
-
-class CustomPrintPageRenderer: UIPrintPageRenderer {
-    
-    let letterPageWidth: CGFloat = 612
-    let letterPageHeight: CGFloat = 792
-
-    override init() {
-        super.init()
-        
-        // Specify the frame of the page.
-        let pageFrame = CGRect(x: 0.0, y: 0.0, width: letterPageWidth, height: letterPageHeight)
-        
-        // Set the page frame.
-        self.setValue(NSValue(CGRect: pageFrame), forKey: "paperRect")
-        
-        // Set the horizontal and vertical insets (that's optional).
-        self.setValue(NSValue(CGRect: pageFrame), forKey: "printableRect")
-    }
-
-}
+//    override init() {
+//        super.init()
+//        
+//        // Specify the frame of the page.
+//        let pageFrame = CGRect(x: 0.0, y: 0.0, width: letterPageWidth, height: letterPageHeight)
+//        
+//        // Set the page frame.
+//        self.setValue(NSValue(CGRect: pageFrame), forKey: "paperRect")
+//        
+//        // Set the horizontal and vertical insets (that's optional).
+//        self.setValue(NSValue(CGRect: pageFrame), forKey: "printableRect")
+//    }
+//
+//}
