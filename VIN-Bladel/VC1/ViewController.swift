ad037971749aacedc8b8cd5c35c88cc3e9bd3f92
Joel Koreth <jkoreth8180@jh-mba158-24.d214.org>
2018-05-11 12:21:12 -0500

    Hides backbutton

--- VIN-Bladel/VC1/ViewController.swift
+++ VIN-Bladel/VC1/ViewController.swift
@@ -17,10 +17,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var scanButton: UIButton!
     @IBOutlet weak var manualButton: UIButton!
     @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet weak var navigationBar: UINavigationItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationController?.navigationItem.hidesBackButton = true
+        navigationBar.hidesBackButton = true
         
         scanButton.backgroundColor = UIColor(red:0.32, green:0.58, blue:0.79, alpha:1.0)
         scanButton.layer.cornerRadius = 40
